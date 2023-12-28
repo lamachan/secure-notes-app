27bdf29914b754eebc7d8c5f83fef6ec6494f034
lamachan <95586117+lamachan@users.noreply.github.com>
2023-12-28 22:21:29 +0100

    First structure tries

---
+++ flask-app/config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
